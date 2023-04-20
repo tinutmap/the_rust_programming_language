2bd2c81511a9a2edac6c8ca57a968cfadce0da58
Tin Dinh <tinutmap@gmail.com>
2023-04-19 21:27:16 -0400

    3rd try

--- c_3_1_variables_mutability/src/main.rs
+++ c_3_1_variables_mutability/src/main.rs
@@ -1,16 +1,27 @@
 fn main() {
+    // // 1st try
     // let mut x = 5;
     // println!("The value of x is {x}");
     // x = 6;
     // println!("The value of x is {x}");
 
-    let x = 5;
+    // // 2nd try
+    // let x = 5;
 
-    let x = x + 1;
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is {x}");
-    }
+    // let x = x + 1;
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is {x}");
+    // }
 
-    println!("The value of x is: {x}")
+    // println!("The value of x is: {x}")
+
+    // 3rd try
+    let spaces = "     ";
+    let spaces = spaces.len();
+    println!("The number of space is {spaces}");
+
+    // // Below will fail due to mismatch type of spaces when making it mutable.
+    // let mut spaces = "     ";
+    // spaces = spaces.len();
 }
